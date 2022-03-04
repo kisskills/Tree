919e83a4d40bb5ece0a5105010dd326844ac2904
Алексей Лабзунов <aleksejlabzunov@iMac-Aleksej.local>
2022-03-04 23:30:01 +0300

    orders

--- binary_tree/tree.c
+++ binary_tree/tree.c
@@ -9,6 +9,7 @@ struct tree {
     struct tree *right;
     struct tree *parent;
 };
+
 /*
 struct tree* create_tree(struct tree* root, int64_t key) {
     struct tree* tmp = malloc(sizeof(struct tree));
@@ -42,19 +43,21 @@ struct tree* add_node(struct tree* root, int64_t key) {
     return root;
 }
 */
-
+// добавление элемента
 void add_node(struct tree** root,struct tree* parent, int64_t key) {
     if (*root == NULL) {
         *root = malloc(sizeof(struct tree));
         (*root)->key = key;
         (*root)->left = (*root)->right = NULL;
         (*root)->parent = parent;
-    } else {
-        if(key < (*root)->key) add_node(&((*root)->left), *root, key);
-        if(key > (*root)->key) add_node(&((*root)->right), *root, key);
     }
+    if(key < (*root)->key)
+        add_node(&((*root)->left), *root, key);
+    else if(key > (*root)->key)
+        add_node(&((*root)->right), *root, key);
+    else {}
 }
-
+// поиск элемента по ключу
 struct tree* find_node(struct tree* root, int64_t key) {
     if (root == NULL || root->key == key)
         return root;
@@ -63,13 +66,13 @@ struct tree* find_node(struct tree* root, int64_t key) {
     else
         return find_node(root->right, key);
 }
-
+// поиск минимального элемента
 struct tree* min(struct tree* root) {
     if (root->left == NULL)
         return root;
     return min(root->left);
 }
-
+// удаление элемента
 struct tree* rm_node(struct tree* root, int64_t key) {
     struct tree* ptr = find_node(root, key);
     if (ptr) {
@@ -82,20 +85,17 @@ struct tree* rm_node(struct tree* root, int64_t key) {
                 ptr->parent->left = ptr->right;
             else
                 ptr->parent->right = ptr->right;
-            // free(ptr);
         } else if (ptr->left != NULL && ptr->right == NULL) {
             ptr->left->parent = ptr->parent;
             if (ptr == ptr->parent->left)
                 ptr->parent->left = ptr->left;
             else
                 ptr->parent->right = ptr->left;
-            // free(ptr);
         } else {
             if (ptr == ptr->parent->left)
                 ptr->parent->left = NULL;
             else 
                 ptr->parent->right = NULL;
-            // free(ptr);
         }
         free(ptr);
         return root;
@@ -103,22 +103,38 @@ struct tree* rm_node(struct tree* root, int64_t key) {
         return root;
     }
 }
-
-void print_tree(struct tree* root) {
-    if (root->left != NULL) print_tree(root->left);
-    if (root->right != NULL) print_tree(root->right);
+// обход дерева корень -> левая ветвь -> правая ветвь
+void pre_order(struct tree* root) {
+    if (root) {
+        printf("%"PRId64" ", root->key);
+        pre_order(root->left);
+        pre_order(root->right);
+    }
+}
+// обход дерева левая ветвь -> корень -> правая ветвь
+void in_order(struct tree* root) {
+    if (root) {
+        in_order(root->left);
+        printf("%"PRId64" ", root->key);
+        in_order(root->right);
+    }
+}
+// обход дерева левая ветвь -> правая ветвь -> корень
+void post_order(struct tree* root) {
+    if (root->left != NULL) post_order(root->left);
+    if (root->right != NULL) post_order(root->right);
     printf("%"PRId64" ", root->key);
 }
-
-void destroy_tree(struct tree* root) {
-    if(root->left != NULL) destroy_tree(root->left);
-    if(root->right != NULL) destroy_tree(root->right);
-    free(root);
+// удаление дерева
+void destroy_tree(struct tree** root) {
+    if((*root)->left != NULL) destroy_tree(&((*root)->left));
+    if((*root)->right != NULL) destroy_tree(&((*root)->right));
+    free(*root);
 }
+
 int main() {
     struct tree* root = NULL;
     struct tree* parent = NULL;
-    // root = create_tree(root, 2);
     add_node(&root, parent, 9);
     add_node(&root, parent, 3);
     add_node(&root, parent, 7);
@@ -127,15 +143,22 @@ int main() {
     add_node(&root, parent, 6);
     add_node(&root, parent, 1);
     add_node(&root, parent, 0);
-    print_tree(root);
+    add_node(&root, parent, 11);
+    post_order(root);
     printf("\n");
 
     rm_node(root, 3);
     rm_node(root, 900);
-    print_tree(root);
-    printf("\n");
 
-    destroy_tree(root);
+    pre_order(root);
+    printf(" pre\n");
+    in_order(root);
+    printf(" in\n");
+    post_order(root);
+    printf(" post\n");
+
+    destroy_tree(&root);
+    root = NULL;
     return 0;
 }
 
